22ca6454e5651606c7fa021ca967dbe5e2840376
OlhaLu <olhalutsyshyna@gmail.com>
2019-11-28 17:54:40 +0200

    Made architecture app

--- src/components/Values/Values.js
+++ src/components/Values/Values.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Value from './Value';
+import Value from '../Value';
 
 const Container = styled.section`
   display: inline-flex;
